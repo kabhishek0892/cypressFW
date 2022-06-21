b237db52af113f1cc543716d1b69e6aa728d44ca
Abhishek Kumar <abhishek.kumar3@go-mmt.com>
2022-06-21 14:39:53 +0530

    Changing configs after upgrade to cypress 10 Q2T-209
    Change-Id: Ie2affd614fa243e952246d025eb52a3d160efeba

---
+++ cypress.config.js
@@ -0,0 +1,34 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  pageLoadTimeout: 10000,
+  clientRoute: '/',
+  projectId: '1234',
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/report',
+    charts: true,
+    reportPageTitle: 'Q2T Automation Report',
+    reportFilename: '[status]_[datetime]-[name]-report',
+    timestamp: 'longDate',
+    overwrite: false,
+    code: true,
+    autoOpen: true,
+  },
+  video: false,
+  env: {
+    username: 'Abhishek',
+    password: 'Test@123',
+    email: 'Abhishek.kumar3@go-mmt.com',
+    APIURL: 'http://corporate-api.quest2travel.org',
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://corporate.quest2travel.org',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+})
